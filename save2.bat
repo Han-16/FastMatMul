a4bc2db7a11aa135bbd95e11018bcecb448c8b2d
lysias9049 <sklee63kr@gmail.com>
2026-05-18 19:11:43 +0900

    자동 저장

---
+++ save2.bat
@@ -0,0 +1,6 @@
+@echo off 
+cd "C:\Users\이경태\FastMatMul" 
+git add . 
+git commit -m "자동 저장" 
+git push 
+echo FastMatMul 저장 완료 
---
+++ start2.bat
@@ -0,0 +1,4 @@
+@echo off 
+cd "C:\Users\이경태\FastMatMul" 
+git pull 
+echo FastMatMul 업데이트 완료 
